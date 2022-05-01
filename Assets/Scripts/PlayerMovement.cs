83ed39677527286318f615bd38ab950d8f2c0b00
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-05-02 01:38:30 +0200

    Changed player control
    Changed player control to following the mouse instead of left and right arrow keys

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject cam;
+
     private float horizontalInput;
-    private float movementBorder = 70;
+    private float horizontalMovementBorder = 70;
+    private float verticalMovementBorderTop = 20;
+    private float verticalMovementBorderBottom = -40;
+    private Vector3 mousePos;
+    private float distToCamera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Calculate distance to vamera to acurately follow the curser on the screen
+        distToCamera = cam.transform.position.y - transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move Player left and right
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * 70);
+        
+        // Make the player follow the mouse curser
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3( mousePos.x, mousePos.y, distToCamera));
+        transform.position = Vector3.Lerp(transform.position, mousePos, 0.3f);
 
         // Stop the player from moving too far
-        if(transform.position.x < - movementBorder)
+        if(transform.position.x < - horizontalMovementBorder)
         {
-            transform.position = new Vector3(-movementBorder, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-horizontalMovementBorder, transform.position.y, transform.position.z);
         }
-        if(transform.position.x > movementBorder)
+        if(transform.position.x > horizontalMovementBorder)
         {
-            transform.position = new Vector3(movementBorder, transform.position.y, transform.position.z);
+            transform.position = new Vector3(horizontalMovementBorder, transform.position.y, transform.position.z);
+        }
+        if(transform.position.z > verticalMovementBorderTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, verticalMovementBorderTop);
+        }
+        if(transform.position.z < verticalMovementBorderBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, verticalMovementBorderBottom);
         }
     }
 }
