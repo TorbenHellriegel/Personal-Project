ac08787bf93a06c4f09e7a522b65c5b618b6b73b
TorbenHellriegel <torbenhellriegel@gmail.com>
2022-05-14 16:35:21 +0200

    Refactoring

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
+    // Variables for enemy movement
     public float horizontalSpeed = 10;
     public float movementDirection = 1;
     private float movementBorder = 70;
+
+    // Variables for shooting
+    public GameObject projectile;
+    public float shootingSpeed = 0.5f;
+    
+    // Variables for enemy health
+    public float maxHealth = 3;
+    public float currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         movementDirection = (Random.Range(0, 2) - 0.5f) * 2;
+
+        currentHealth = maxHealth;
+
+        Invoke("Shoot", shootingSpeed);
     }
 
     // Update is called once per frame
@@ -31,6 +44,31 @@ public class EnemyMovement : MonoBehaviour
             transform.position = new Vector3(movementBorder, transform.position.y, transform.position.z);
             movementDirection = movementDirection * -1;
         }
-        
+    }
+
+    // Repetedly shoots projectiles
+    private void Shoot()
+    {
+        Instantiate(projectile, transform.position, transform.rotation);
+        Invoke("Shoot", shootingSpeed);
+    }
+
+    // Checks for collisions with projectiles
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("playerShot"))
+        {
+            loseHealth(1);
+        }
+    }
+
+    // Loses health when hit by projectile
+    private void loseHealth(float amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    // Variables for player movement
     public GameObject cam;
-
     private float horizontalInput;
     private float horizontalMovementBorder = 70;
     private float verticalMovementBorderTop = 20;
@@ -13,17 +13,29 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 mousePos;
     private float distToCamera;
 
+    // Variables for shooting
+    public GameObject projectile;
+    public float shootingSpeed = 0.1f;
+
+    // Variables for player health
+    public float maxHealth = 10;
+    public float currentHealth;
+
+
     // Start is called before the first frame update
     void Start()
     {
         // Calculate distance to vamera to acurately follow the curser on the screen
         distToCamera = cam.transform.position.y - transform.position.y;
+
+        currentHealth = maxHealth;
+
+        Invoke("Shoot", shootingSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         // Make the player follow the mouse curser
         mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(new Vector3( mousePos.x, mousePos.y, distToCamera));
@@ -45,6 +57,33 @@ public class PlayerMovement : MonoBehaviour
         if(transform.position.z < verticalMovementBorderBottom)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, verticalMovementBorderBottom);
+        }
+    }
+
+    // Repetedly shoots projectiles
+    private void Shoot()
+    {
+        Instantiate(projectile, transform.position, transform.rotation);
+        Invoke("Shoot", shootingSpeed);
+    }
+
+    // Checks for collisions with projectiles
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("enemyShot"))
+        {
+            loseHealth(1);
+            Destroy(other.gameObject);
+        }
+    }
+
+    // Loses health when hit by projectile
+    private void loseHealth(float amount)
+    {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
